cf96dc59f3b7410742d788a35989d2a30fc22b5a
devarajvarsha <devarajvarshaa@gmail.com>
2025-01-05 17:42:25 +0530

    add

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
-const API_KEY = 'db95f6a24896411da96e8f05cb8fff08';  // Replace this with your actual News API key
-const API_URL = 'https://newsapi.org/v2/top-headlines?apiKey=' + API_KEY + '&category=';
+const apiKey = "064037c5403e24295ce5bd1ae6c93a26";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const loadingElement = document.getElementById('loading');
-const articlesElement = document.getElementById('articles');
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-function fetchNews(category) {
-  loadingElement.style.display = 'block';
-  articlesElement.innerHTML = '';
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  fetch(API_URL + category)
-    .then(response => response.json())
-    .then(data => {
-      loadingElement.style.display = 'none';
-      if (data.articles && data.articles.length > 0) {
-        data.articles.forEach(article => {
-          displayArticle(article);
-        });
-      } else {
-        articlesElement.innerHTML = '<p>No articles found.</p>';
-      }
-    })
-    .catch(error => {
-      loadingElement.style.display = 'none';
-      articlesElement.innerHTML = '<p>Error fetching the news.</p>';
-      console.error('Error fetching news:', error);
-    });
+  if(response.status==404){
+    document.querySelector(".error").style.display = 'block';
+    document.querySelector(".weather").style.display = "none";
+  }else{
+    var data = await response.json();
+  
+
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+
+    if(data.weather[0].main=='Clouds'){
+      weatherIcon.src = "images/clouds.png";
+    }else if(data.weather[0].main=='Clear'){
+      weatherIcon.src = "images/clear.png";
+    }else if(data.weather[0].main=='Rain'){
+      weatherIcon.src = "images/rain.png";
+    }else if(data.weather[0].main=='Drizzle'){
+      weatherIcon.src = "images/drizzle.png";
+    }else if(data.weather[0].main=='Mist'){
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    
+
+  }
+
+  
+
 }
 
-function displayArticle(article) {
-  const articleCard = document.createElement('div');
-  articleCard.classList.add('article-card');
 
-  const articleImage = article.urlToImage ? `<img src="${article.urlToImage}" alt="${article.title}">` : '';
-  const articleTitle = article.title ? `<h3><a href="${article.url}" target="_blank">${article.title}</a></h3>` : '';
-  const articleDescription = article.description ? `<p>${article.description}</p>` : '';
-
-  articleCard.innerHTML = `
-    ${articleImage}
-    <div class="content">
-      ${articleTitle}
-      ${articleDescription}
-    </div>
-  `;
-
-  articlesElement.appendChild(articleCard);
-}
-
-// Load default category (Top Headlines)
-fetchNews('general');
+searchBtn.addEventListener("click", ()=>{
+  checkWeather(searchBox.value);
+})
 
 
 
